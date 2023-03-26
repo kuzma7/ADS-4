9d64ece66c9c482cb30d6f0ab224250005440d62
kuzma7 <122277574+kuzma7@users.noreply.github.com>
2023-03-26 19:43:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
+        for (int j = i+1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
             }
@@ -11,38 +11,46 @@ int countPairs1(int *arr, int len, int value) {
     return count;
 }
 
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int* arr, int len, int value) {
     int count = 0;
-    int left = 0;
-    int right = len - 1;
-    while (left < right) {
-        if (arr[left] + arr[right] == value) {
+    int i = 0;
+    int j = len - 1;
+    while (i < j) {
+        int sum = arr[i] + arr[j];
+        if (sum == value) {
             count++;
-            left++;
-            right--;
-        } else if (arr[left] + arr[right] < value) {
-            left++;
-        } else {
-            right--;
+            i++;
+            j--;
+        }
+        else if (sum > value) {
+            j--;
+        }
+        else {
+            i++;
         }
     }
     return count;
 }
 
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int* arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        int left = i + 1;
-        int right = len - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] + arr[i] == value) {
-                count++;
+        int diff = value - arr[i];
+        int low = i + 1;
+        int high = len - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == diff) {
+                if (arr[i] != arr[mid]) {
+                    count++;
+                }
                 break;
-            } else if (arr[mid] + arr[i] < value) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            }
+            else if (arr[mid] < diff) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
             }
         }
     }

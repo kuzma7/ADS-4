db0cc59a40c3bfbf545c7de25f8d058519efdfa4
kuzma7 <122277574+kuzma7@users.noreply.github.com>
2023-03-26 20:31:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,60 +1,53 @@
 // Copyright 2021 NNTU-CS
-int SearchBin(int* arr, int left, int right, int value) {
-    int anyu = 0;
-    while (left <= right) {
-        int middle = (left + right) / 2;
-        if (arr[middle] == value) {
-            anyu++;
-            int temp = middle - 1;
-            while (temp >= left && arr[temp] == value) {
-                anyu++;
-                temp--;
-            }
-            temp = middle + 1;
-            while (temp <= right && arr[temp] == value) {
-                anyu++;
-                temp++;
-            }
-            break;
-        }
-        if (arr[middle] < value) {
-            left = middle + 1;
-        }
-        if (arr[middle] > value) {
-            right = middle - 1;
-        }
-    }
-    return anyu;
-}
-
 int countPairs1(int* arr, int len, int value) {
-    int anyu = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
+    int detect = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
-                anyu = anyu + 1;
+                detect += 1;
             }
         }
     }
-    return anyu;
+    return detect;
 }
 int countPairs2(int* arr, int len, int value) {
-    int anyu = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = len - 1; i < j; --j) {
+    int detect = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = len - 1; i < j; j--) {
             if (arr[i] + arr[j] == value) {
-                anyu = anyu + 1;
+                detect += 1;
             }
         }
     }
-    return anyu;
+    return detect;
 }
 int countPairs3(int* arr, int len, int value) {
-    int anyu = 0;
-    int value1 = 0;
-    for (int i = 0; i < len - 1; ++i) {
-        value1 = value - arr[i];
-        anyu = anyu + SearchBin(arr, i, len, value1);
+    int detect = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int l = i, r = len;
+        while (l < r - 1) {
+            int val = (l + r) / 2;
+            if (arr[i] + arr[val] == value) {
+                detect += 1;
+                int NewVal = val + 1;
+                while (arr[i] + arr[NewVal] == value && NewVal < r) {
+                    detect += 1;
+                    NewVal += 1;
+                }
+                NewVal = val - 1;
+                while (arr[i] + arr[NewVal] == value && NewVal > l) {
+                    detect += 1;
+                    NewVal -= 1;
+                }
+                break;
+            }
+            if (arr[i] + arr[val] > value) {
+                r = val;
+            }
+            else {
+                l = val;
+            }
+        }
     }
-    return anyu;
+    return detect;
 }

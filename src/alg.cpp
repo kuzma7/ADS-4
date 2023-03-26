8d49b4eb033b57bcd62fe8ac6ec8feb5cad5e3ab
kuzma7 <122277574+kuzma7@users.noreply.github.com>
2023-03-26 19:51:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,58 +1,61 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i+1; j < len; j++) {
+int SearchBin(int* arr, int left, int right, int value) {
+    int anyu = 0;
+    int middle = 0;
+    int temp = 0;
+    while (right - left > 1) {
+        middle = (left + right) / 2;
+        if (arr[middle] == value) {
+            anyu = anyu + 1;
+            temp = middle + 1;
+            while (arr[temp] == value && temp < right) {
+                anyu = anyu + 1;
+                temp = temp + 1;
+            }
+            temp = middle - 1;
+            while (arr[temp] == value && temp > left) {
+                anyu = anyu + 1;
+                temp = temp - 1;
+            }
+            break;
+        }
+        if (arr[middle] < value) {
+            left = middle;
+        }
+        if (arr[middle] > value) {
+            right = middle;
+        }
+    }
+    return anyu;
+}
+int countPairs1(int* arr, int len, int value) {
+    int anyu = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
             if (arr[i] + arr[j] == value) {
-                count++;
+                anyu = anyu + 1;
             }
         }
     }
-    return count;
+    return anyu;
 }
-
 int countPairs2(int* arr, int len, int value) {
-    int count = 0;
-    int i = 0;
-    int j = len - 1;
-    while (i < j) {
-        int sum = arr[i] + arr[j];
-        if (sum == value) {
-            count++;
-            i++;
-            j--;
-        }
-        else if (sum > value) {
-            j--;
-        }
-        else {
-            i++;
+    int anyu = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = len - 1; i < j; --j) {
+            if (arr[i] + arr[j] == value) {
+                anyu = anyu + 1;
+            }
         }
     }
-    return count;
+    return anyu;
 }
-
 int countPairs3(int* arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        int diff = value - arr[i];
-        int low = i + 1;
-        int high = len - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] == diff) {
-                if (arr[i] != arr[mid]) {
-                    count++;
-                }
-                break;
-            }
-            else if (arr[mid] < diff) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
+    int anyu = 0;
+    int value1 = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        value1 = value - arr[i];
+        anyu = anyu + SearchBin(arr, i, len, value1);
     }
-    return count;
+    return anyu;
 }
